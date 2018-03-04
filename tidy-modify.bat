5195c52cecdfd70286a5a14198b74c8e9c46754f
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-04 17:43:57 +0300

    ci(appveyor): Force exit 0

---
+++ tidy-modify.bat
@@ -0,0 +1,9 @@
+@REM @Author: Kristinita
+@REM @Date:   2018-03-04 17:38:29
+@REM @Last Modified by:   Kristinita
+@REM Modified time: 2018-03-04 17:43:04
+@echo off
+tidy -mq SashaBeautiful.html
+@rem https://stackoverflow.com/a/4728151/5951529
+@rem https://stackoverflow.com/a/334893/5951529
+exit /b 0
