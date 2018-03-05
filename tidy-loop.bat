e78e8458d038611d6f78a5eabc79e3f0a09f1558
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-05 13:21:25 +0300

    Test

--- tidy-loop.bat
+++ tidy-loop.bat
@@ -1,26 +1,26 @@
 @REM @Author: Kristinita
 @REM @Date:   2018-03-04 17:48:14
 @REM @Last Modified by:   Kristinita
-@REM Modified time: 2018-03-05 08:53:49
+@REM Modified time: 2018-03-05 09:59:40
 @rem If/else exitcode:
 @REM https://stackoverflow.com/a/36897449/5951529
 @echo off
-set OK=true
+set VALIDALL=true
 @REM Correct ERRORLEVEL in IF/ELSE
 @REM https://stackoverflow.com/a/4368104/5951529
 setlocal EnableDelayedExpansion
-FOR /R %%i IN (*.html) DO (
-	tidy -mq --wrap 0 %%i
+FOR /R %%f IN (*.html) DO (
+	tidy -mq --wrap 0 %%f
 	IF "!ERRORLEVEL!"=="0" (
 		@REM Split filename from filepath
 		@REM https://stackoverflow.com/a/9253018/5951529
-		echo %%~nxi is valid!
+		echo %%~nxf is VALIDALL!
 	) ELSE (
 	@rem Exclamation mark as literal:
 	@rem https://stackoverflow.com/a/3288632/5951529
-	echo You have problems in %%~nxi. Please, correct them^^!
-	set OK=false
+	echo You have problems in %%~nxf. Please, correct them^^!
+	set VALIDALL=false
 	)
 )
-echo %OK%
-%OK%
+echo %VALIDALL%
+%VALIDALL%
