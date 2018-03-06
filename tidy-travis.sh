b534c7762c65d1d9496b4123588dad4054e45f37
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-06 18:56:30 +0300

    ci(travis): Cyrillic filename invalid file

--- tidy-travis.sh
+++ tidy-travis.sh
@@ -1,14 +1,15 @@
 # @Author: Kristinita
 # @Date:   2018-03-06 18:34:40
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-03-06 18:40:25
+# @Last Modified time: 2018-03-06 18:53:58
 shopt -s globstar
 ok=true
 for i in *.html; do
-  if tidy -mq "$i"; then
-    printf '%s\n' "$i"
+  if tidy -mq $i; then
+    echo $i valid!
   else
     ok=false
+    echo $i not valid!
   fi
 done
-"$ok"
+$ok
