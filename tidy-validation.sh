088a17d032ef689f9e2530178219185ae495ff95
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-06 19:32:20 +0300

    ci(travis): Quiet validation

--- tidy-validation.sh
+++ tidy-validation.sh
@@ -1,8 +1,8 @@
 # @Author: Kristinita
 # @Date:   2018-03-06 18:57:19
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-03-06 18:59:53
+# @Last Modified time: 2018-03-06 19:10:04
 for i in *.html;
-	do tidy -mq $i
+	do tidy -mq --show-errors 0 --show-warnings 0 $i
 done
 true
